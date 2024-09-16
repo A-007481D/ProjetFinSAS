4a39b93b0f80e75ae471e05e7d813c852d9d9e42
A-007481D LB <labidabdelmalek@gmail.com>
2024-09-16 16:36:48 +0000

    add student & dept func

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 #include<string.h>
+#define max_etds 100
+#define informatique 1
+#define sciences 2
+#define gestion 3
+#define art_design 4
 
 typedef struct
 {
@@ -12,7 +17,69 @@ typedef struct
 } Etudiant;
 
 Etudiant etudiant_t[max_etds];
-int nbrEtds = 8;
+int nbrEtds = 0;
+
+void afficher_menu()
+{
+    printf("\n+=============================================================+");
+    printf("\n|              ---------Menu Principal------------             |");
+    printf("\n+=============================================================+\n");
+    printf(" 1=> Ajouter un etudiant                                       |\n");
+    printf(" 2=> Afficher les details d'un etudiant                        |\n");
+    printf(" 3=> Rechercher un etudiant                                    |\n");
+    printf(" 4=> Modifier ou supprimer un etudiant                         |\n");
+    printf(" 5=> Trier les etudiants                                       |\n");
+    printf(" 6=> Calculer la moyenne generale                              |\n");
+    printf(" 7=> Statistiques                                              |\n");
+    printf(" 0=> Quitter                                                   |\n");
+    printf("+=============================================================+\n");
+}
+
+int departement(char depart[])
+{
+    int choix;
+    printf("\n+=============================================================+");
+    printf("\n|              --------Departement---------                   |");
+    printf("\n+=============================================================+\n");
+    printf("\n1=> Informatique\n2=> Sciences\n3=> Gestion\n4=> Art et Design\nChoisir votre departement : ");
+    scanf("%d", &choix);
+
+    switch (choix)
+    {
+    case informatique: strcpy(depart, "Informatique"); break;
+    case sciences: strcpy(depart, "Sciences"); break;
+    case gestion: strcpy(depart, "Gestion"); break;
+    case art_design: strcpy(depart, "Art et Design"); break;
+    default: printf("choix invalide, essayer a nouveau!");
+    departement(depart);
+    }
+    return 0;
+}
+
+void ajouter_etudiant()
+{
+
+    printf("Entrer les informations de l'etudiant:\n");
+    printf("Numero unique : ");
+    scanf("%d", &etudiant_t[nbrEtds].numUnique);
+    printf("Nom : ");
+    scanf("%s", etudiant_t[nbrEtds].nom);
+    printf("Prenom : ");
+    scanf("%s", etudiant_t[nbrEtds].prenom);
+    printf("Date de naissance (jour-mois-annee) : ");
+    scanf("%s", etudiant_t[nbrEtds].dateNaissance);
+
+    departement(etudiant_t[nbrEtds].departement);
+    do
+    {
+        printf("Note Generale entre 0 et 20: ");
+        scanf("%f", &etudiant_t[nbrEtds].noteGeneral);
+    } while (etudiant_t[nbrEtds].noteGeneral < 0 || etudiant_t[nbrEtds].noteGeneral > 20);
+
+    printf("Etudiant '%s' a ete ajoute avec succes nbr (%d).\n", etudiant_t[nbrEtds].nom, nbrEtds);
+    nbrEtds++;
+}
+
 
 int main()
 {
@@ -25,7 +92,7 @@ int main()
         scanf("%d", &choix);
         switch (choix)
         {
-        case 1: 
+        case 1: ajouter_etudiant(); break;
         case 2: 
         case 3: 
         case 4: 
